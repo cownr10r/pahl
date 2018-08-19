1aefaa25abfb76d7040c79ca6c04d729a13bf8bc
cownr10r <mario.a.martinez@live.com>
2018-08-19 14:24:03 -0500

    Save the output with fewer keystrokes

---
+++ R/filesave.R
@@ -0,0 +1,14 @@
+#' Save the Output
+#'
+#' @param save_dir The directory path.
+#' @param file_name The name of the file (without extension or dot), in quotes.
+#' @return saved evidence in an RDS file to capture artifacts from the probabilistic event.
+#' @export
+#' @examples
+#' filesave(data_dir, "case1")
+
+
+filesave <- function(save_dir = in_quotes, file_name = in_quotes) {
+         	case <- list(topics, school, work, family)
+         	saveRDS(case, paste0(save_dir, file_name, ".RDS"))
+         }
