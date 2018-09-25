b452b86ed9281eba800e18d9075e322a7757c49e
cownr10r <mario.a.martinez@live.com>
2018-09-25 11:44:00 -0500

    clarifying which quanteda

--- R/ldamodel.R
+++ R/ldamodel.R
@@ -9,11 +9,11 @@
 #' mod <- ldamodel(features, 8)
 
 
-ldamodel <- function(a = features, b = 0, seed = 0){
+ldamodel <- function(a = features, b = 0, c = 0){
         gibbs <- topicmodels::LDA(a, b, method = "Gibbs", control = list(seed = SEED, burnin = 1000, thin = 100, iter = 1000))
         words <- terms(gibbs,30) %>% data.frame(., stringsAsFactors=F)
         kappa <- b
         seeds <- gibbs@control@seed
-        result <- list(gibbs=gibbs, words = words, seed = seed, kappa = kappa, text = text)
+        result <- list(gibbs=gibbs, words = words, seed = c, kappa = kappa, text = text)
         return(result)
 }
