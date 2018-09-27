09c855fd21c6f88bcc48c74a8da0b62d1492fc7a
cownr10r <mario.a.martinez@live.com>
2018-09-27 09:14:26 -0500

    create a variable to enter column number into the .xlsx file

--- R/essay.R
+++ R/essay.R
@@ -3,15 +3,16 @@
 #' @param a The file directory holding the data set
 #' @param b The file in question
 #' @param d The case number
+#' @param e The row number for texts
 #' @return The corpus object
 #' @export
 #' @examples
 #' doc <- essay(data_dir, file, 1)
 
 
-essay <- function(a = data_dir, b = file, d = 0){
+essay <- function(a = data_dir, b = file, d = 0, e = 0){
         composition <- openxlsx::read.xlsx(paste0(a, b)) %>%
-                .[d, c(d,23)] %>%
+                .[d, d:e] %>%
                 quanteda::corpus(., text_field = "section3")
         return(composition)
 }
